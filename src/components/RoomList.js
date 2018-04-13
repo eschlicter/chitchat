458330294c2d2f57cc5b682a202b4b6af6f833a7
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-04-13 09:50:59 -0700

    fixed error room delete

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -43,7 +43,7 @@ class RoomList extends Component {
   }
 
   deleteRoom(roomKey) {
-    const room = this.props.firebase().ref('rooms' + roomKey);
+    const room = this.props.firebase.database().ref('rooms' + roomKey);
     room.remove();
     const remainingRooms = this.state.rooms
     .filter(room => room.key !== roomKey);
